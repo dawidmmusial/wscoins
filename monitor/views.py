8bb21e6affc50c7c5fdf0e577d894224191cef28
Dawid <dawid.m.musial@gmail.com>
2018-04-27 10:23:20 +0000

    Correct display cryprocurrency values at start page

--- monitor/views.py
+++ monitor/views.py
@@ -1,5 +1,12 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from .tools import get_cryptocurrency
 
 class MonitorView(TemplateView):
-	template_name = 'monitor.html'
+    template_name = 'monitor.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cryptocurrency'] = get_cryptocurrency(settings.CRYPTOCOMPARE_API_PRICES)
+        return context
